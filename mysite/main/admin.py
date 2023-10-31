7c5612e8329dc599216262ea9fa6c0f5db278d32
AsahiSoftWareEngineer <atyapi826@icloud.com>
2023-10-31 17:10:27 +0900

    ブログそのものの追加

--- mysite/main/admin.py
+++ mysite/main/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import AppModel, ContentModel, PageModel, PublishedItemModel
+from .models import AppModel, ContentModel, PageModel, PublishedItemModel, PublishedModel
 admin.site.register(AppModel)
 admin.site.register(ContentModel)
 admin.site.register(PageModel)
 admin.site.register(PublishedItemModel)
+admin.site.register(PublishedModel)
 # Register your models here.
